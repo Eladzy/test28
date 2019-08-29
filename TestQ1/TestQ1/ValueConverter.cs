30f8b7d1c75b4c199d35c38f69a6562df2918f75
Eladzy <45952716+Eladzy@users.noreply.github.com>
2019-08-29 13:39:06 +0300

    Add files via upload

---
+++ TestQ1/TestQ1/ValueConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TestQ1
+{
+    class ValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double sliderValue = (double)value;
+            double sliderMax = 300;
+            if (sliderMax != null && sliderValue != null)
+            {
+                if (sliderValue <= sliderMax * 0.25)
+                {
+                    return "Small";
+                }
+                else if (sliderValue <= sliderMax * 0.5)
+                {
+                    return "Medium";
+                }
+                else if (sliderValue <= sliderMax * 0.75)
+                {
+                    return "Large";
+                }
+                else
+                {
+                    return "Extra Large";
+                }
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
